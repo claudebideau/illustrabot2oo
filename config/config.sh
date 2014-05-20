1f92de7abff1d0a2ab768848b401244b2c920c00
fablab <fablab@homedesk.(none)>
2014-05-20 21:57:26 +0200

    insert config file

---
+++ config/config.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+wget -c https://raw.github.com/RobertCNelson/tools/master/pkgs/dtc.sh
+chmod u+x dtc.sh
+
+cp DM-GPIO-Test-00A0.dtbo /lib/firmware
+	
+export SLOTS=/sys/devices/bone_capemgr.9/slots
+export PINS=/sys/kernel/debug/pinctrl/44e10800.pinmux/pins
+
+cat $SLOTS
+cat $PINS
