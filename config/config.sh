bd57db74d58c7e075848601507a0232a197b0b22
Demo User <debian@arm.(none)>
2014-05-23 19:55:12 +0000

    test 21/05

--- config/config.sh
+++ config/config.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
 
-wget -c https://raw.github.com/RobertCNelson/tools/master/pkgs/dtc.sh
-chmod u+x dtc.sh
+#wget -c https://raw.github.com/RobertCNelson/tools/master/pkgs/dtc.sh
+#chmod u+x dtc.sh
 
-cp DM-GPIO-Test-00A0.dtbo /lib/firmware
+#cp DM-GPIO-Test-00A0.dtbo /lib/firmware
 	
 export SLOTS=/sys/devices/bone_capemgr.9/slots
 export PINS=/sys/kernel/debug/pinctrl/44e10800.pinmux/pins
 
 cat $SLOTS
 cat $PINS
+echo arm_control-1 > $SLOTS
+
